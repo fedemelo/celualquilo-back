c6b801615ee1971eb5033a03b10d60522dd34086
José Bocanegra <j.bocanegra@uniandes.edu.co>
2022-09-05 15:34:46 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
