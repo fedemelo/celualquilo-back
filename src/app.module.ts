1807fb9f2da82598fe84e8279145225a4b247b2f
Mariana Ruiz <m.ruizg@uniandes.edu.co>
2023-11-07 17:11:19 -0500

    Conexion base datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { PhoneModule } from './phone/phone.module';
 import { BrandModule } from './brand/brand.module';
 import { RentModule } from './rent/rent.module';
+import { ReviewModule } from './review/review.module';
+import { UserModule } from './user/user.module';
+import { PhoneEntity } from './phone/phone.entity';
+import { BrandEntity } from './brand/brand.entity';
+import { RentEntity } from './rent/rent.entity';
+import { ReviewEntity } from './review/review.entity';
+import { UserEntity } from './user/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PhoneModule, BrandModule, RentModule],
+  imports: [PhoneModule, BrandModule, RentModule, ReviewModule, UserModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password:'postgres',
+    database: 'celualquilo',
+    entities: [PhoneEntity, BrandEntity, RentEntity, ReviewEntity, UserEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
